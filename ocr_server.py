202180a142195ad3c047dbe499c31ce0a3138d3a
AnastasiaGalenchik <anastasia.galenchik@gmail.com>
2025-05-28 14:40:10 +0500

    Add files via upload

---
+++ ocr_server.py
@@ -0,0 +1,21 @@
+import easyocr
+
+# 1. Настройки
+IMAGE_PATH = input('Введите путь к файлу')  # Путь к вашему PNG-файлу
+OUTPUT_TXT = "output.txt"  # Файл для сохранения результата
+LANGUAGES = ["ru", "en"]  # Языки распознавания
+
+# 2. Инициализация EasyOCR
+reader = easyocr.Reader(LANGUAGES)
+
+# 3. Распознавание текста
+result = reader.readtext(IMAGE_PATH, detail=0,paragraph = True)
+text = " ".join(result)  # Объединяем строки через перенос
+
+# 4. Сохранение в файл
+with open(OUTPUT_TXT, "w", encoding="utf-8") as f:
+    f.write(text)
+
+print(f"Текст успешно сохранён в файл: {OUTPUT_TXT}")
+print("Распознанный текст:")
+print(text)
